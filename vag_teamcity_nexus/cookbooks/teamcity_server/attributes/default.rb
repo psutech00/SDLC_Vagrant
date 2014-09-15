f873b3e98b64de9b674773351d20e16f74d3c23f
jlarriba <juan@jlarriba.es>
2014-09-15 09:42:36 +0200

    Delete everything

--- vag_teamcity_nexus/cookbooks/teamcity_server/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default["teamcity_server"]["version"]               = "7.1.4"
-default["teamcity_server"]["build_agent"]["server"] = nil
-default["teamcity_server"]["build_agent"]["name"]   = nil
-default["teamcity_server"]["server"]["address"]     = "0.0.0.0"
-default["teamcity_server"]["server"]["port"]        = 8111
